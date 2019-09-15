9863cc26c6bf3ac09c83b8cc9a2229f28ab96ae3
zhangmeng <zhangmeng@qq.com>
2019-09-15 20:03:09 +0800

    初始化目录完成

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// 职责配置路由
+import React from 'react'
+import { BrowserRouter as Router , Link , Route } from 'react-router-dom'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+// 导入页面
+// './pages/Home/index.js' .js 后缀 以及 index 都可以省略。
+// 因为在导入模块时，会自动查找 index.js
+import Home from './pages/Home'
+import CityList from './pages/CityList'
 
-export default App;
+// 没有状态的更新用无状态函数组件
+const App = () => {
+    return (
+        <Router>
+        <div className="App">
+        <Link to="/home">首页</Link>
+        <br/>
+        <Link to="/citylist">城市选择</Link>
+        <Route path="/home" component={Home}></Route>
+        <Route path="/citylist" component={CityList}></Route>
+        </div>
+        </Router>
+    )
+} 
+
+export default App
